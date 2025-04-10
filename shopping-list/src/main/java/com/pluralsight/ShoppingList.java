ad5f6951292fed09ec1f4cb0504c9b893daef2fe
Daniel Okonkwo <danielokonkwo.goz@gmail.com>
2025-04-10 11:43:53 -0500

    completed ShoppingList app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping list: ");
+        System.out.println("Apples");
+        System.out.println("Milk");
+        System.out.println("chips");
+        System.out.println("Shoes");
+        System.out.println("Pizza");
+        System.out.println("Candy");
+        System.out.println("Salad");
+        System.out.println("Water");
+        System.out.println("Bread");
+        System.out.println("glasses");
+    }
+}
