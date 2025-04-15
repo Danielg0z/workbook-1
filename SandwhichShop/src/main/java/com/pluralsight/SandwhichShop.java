5be8ce450b92ed7326183e7421be8e2a33a9fa65
Daniel Okonkwo <danielokonkwo.goz@gmail.com>
2025-04-15 12:03:04 -0500

    Completed exercise 1 Sandwhich Shop

---
+++ SandwhichShop/src/main/java/com/pluralsight/SandwhichShop.java
@@ -0,0 +1,47 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwhichShop {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in); // intialized scanner
+
+        System.out.println("Welcome to the Sandwhich Shop!");
+        System.out.println("\nWhat would you like to order? ");
+        System.out.println("\nRegular(R) | Large(L) ");
+
+        String choice = myScanner.nextLine();
+
+        int age = 0;
+        double Regular = 5.45;
+        double Large = 8.95;
+
+        if (choice.equalsIgnoreCase("R")) {
+            System.out.println("How old are you?");
+
+            age = myScanner.nextInt();
+
+            if (age > 18 && age < 64) {
+                System.out.printf("Your total is: $%.2f", Regular);
+            } else if (age <= 17) {
+                System.out.printf("Congrats on your discount! Your total is: $%.2f",(Regular * 0.9));
+            } else if (age >= 65) {
+                System.out.printf("Congrats on your discount! Your total is: $%.2f",(Regular * 0.8));
+            }
+
+        } else if (choice.equalsIgnoreCase("L")) {
+            System.out.println("How old are you?\n");
+
+            age = myScanner.nextInt();
+
+            if (age > 18 && age < 64) {
+                System.out.printf("Your total is: $%.2f", Large);
+            } else if (age <= 17) {
+                System.out.printf("Congrats on your discount! Your total is: $%.2f",(Large * 0.9));
+            } else if (age >= 65) {
+                System.out.printf("Congrats on your discount ! Your total is: $%.2f",(Large * 0.8));
+            }
+
+        }
+    }
+}
